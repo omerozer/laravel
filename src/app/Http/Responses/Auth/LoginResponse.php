fbe8a68c143feaeca49dfd8e46fdfcf961d167e9
omerozer <omerozer88@gmail.com>
2026-02-26 15:42:41 +0300

    up

---
+++ src/app/Http/Responses/Auth/LoginResponse.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Responses\Auth;
+
+use Filament\Auth\Http\Responses\Contracts\LoginResponse as LoginResponseContract;
+use Illuminate\Http\RedirectResponse;
+
+class LoginResponse implements LoginResponseContract
+{
+    public function toResponse($request): RedirectResponse
+    {
+        return redirect()->intended('/anasayfa');
+    }
+}
--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Http\Responses\Auth\LoginResponse;
+use Filament\Auth\Http\Responses\Contracts\LoginResponse as LoginResponseContract;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->bind(LoginResponseContract::class, LoginResponse::class);
     }
 }

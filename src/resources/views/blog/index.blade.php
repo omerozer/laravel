519cf63ac32a68577770bf021c54e1c8fa83ecd2
omerozer <omerozer88@gmail.com>
2026-02-28 00:01:15 +0300

    up

--- src/resources/views/blog/index.blade.php
+++ src/resources/views/blog/index.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Blog - {{ config('app.name') }}</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="h-full bg-gray-50 dark:bg-gray-950 font-sans antialiased">

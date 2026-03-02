40c2e54f56ad27093d788c5b66fdab8507c66355
omerozer <omerozer88@gmail.com>
2026-03-03 02:12:03 +0300

    up

--- src/routes/web.php
+++ src/routes/web.php
@@ -58,7 +58,5 @@ Route::view('/component', 'components')->name('components.gallery');
 
 Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
 Route::get('/blog/kategori/{slug}', [BlogController::class, 'category'])->name('blog.category');
-Route::redirect('/post/{slug}', '/')->where('slug', '.*');
-Route::get('/blog/{slug}', function (string $slug) {
-    return redirect()->route('blog.show', ['slug' => $slug], 301);
-})->name('blog.show.redirect');
+Route::redirect('/post/{slug}', '/')->where('slug', '.*')->name('blog.show');
+Route::get('/blog/{slug}', fn (string $slug) => redirect('/'))->name('blog.show.redirect');
